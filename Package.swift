badd851700df0f63c4087429a3277d708f79324f
Balaganesh S <balaganesh@juspay.in>
2026-02-27 10:25:38 +0530

    Updated Package for version 2.2.5.10

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperAPay",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.5.9/HyperAPay.zip",
-            checksum: "84147719012c5db6e44b2b6b9ec0083f497e96e867bb8798fb8486f505148b48"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.5.10/HyperAPay.zip",
+            checksum: "2e4fb2ca7aedd60c25003df3bdb93cce9e883860f3a3953c2c072645ce6abc87"
         ),
         .binaryTarget(
             name: "AmazonPayHardenediOSSDK",
